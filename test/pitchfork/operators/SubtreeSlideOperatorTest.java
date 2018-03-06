0ed74725cb427ac7d1d6b79c487d6de731d4d5cd
Tim Vaughan <tgvaughan@gmail.com>
2018-03-06 11:36:18 +0100

    Added some unit tests for STS op with probCoalAttach>0

--- test/pitchfork/operators/SubtreeSlideOperatorTest.java
+++ test/pitchfork/operators/SubtreeSlideOperatorTest.java
@@ -8,6 +8,8 @@ import org.junit.Test;
 
 public class SubtreeSlideOperatorTest {
 
+    /* Tests WITHOUT coalescence node attachment */
+
     @Test
     public void testComputeYoungerAttachmentPointProb1() {
 
@@ -106,6 +108,77 @@ public class SubtreeSlideOperatorTest {
         SubtreeSlideOperator.AttachmentPoint ap =
                 stsOp.getOlderAttachmentPoint(startNode, stsOp.getCurrentLambda());
 
+        System.out.println(ap);
+
+        double logP1 = ap.logProb;
+
+        ap.logProb = 0.0;
+        stsOp.computeOlderAttachmentPointProb(ap, startNode, lambda);
+
+        double logP2 = ap.logProb;
+
+        Assert.assertEquals(logP1, logP2, 1e-10);
+    }
+
+    /* Tests WITH coalescence node attachment */
+
+    @Test
+    public void testGetYoungerAttachmentPoint2() {
+        Tree tree = new TreeParser("((A:1,B:1):1,C:3):0.0");
+
+        SubtreeSlideOperator stsOp = new SubtreeSlideOperator();
+        stsOp.initByName(
+                "tree",tree,
+                "relSize",0.15,
+                "probCoalAttach", 1.0,
+                "weight", 1.0);
+
+        Node edgeBaseNode = tree.getNode(2);
+        Node edgeBaseParent = edgeBaseNode.getParent();
+        double lambda = stsOp.getCurrentLambda();
+
+        SubtreeSlideOperator.AttachmentPoint ap = null;
+
+        boolean succeeded = false;
+        do {
+            try {
+                ap = stsOp.getYoungerAttachmentPoint(edgeBaseNode, edgeBaseParent, lambda);
+                succeeded = true;
+            } catch (SubtreeSlideOperator.AttachmentException ex) {
+            }
+        } while(!succeeded);
+
+        System.out.println(tree);
+        System.out.println(ap);
+
+        double logP1 = ap.logProb;
+
+        ap.logProb = 0.0;
+        stsOp.computeYoungerAttachmentPointProb(ap, edgeBaseParent, lambda);
+
+        double logP2 = ap.logProb;
+
+        Assert.assertEquals(logP1, logP2, 1e-10);
+    }
+
+    @Test
+    public void testGetOlderAttachmentPoint2() {
+        Tree tree = new TreeParser("((A:1,B:1):1,C:3):0.0");
+
+        SubtreeSlideOperator stsOp = new SubtreeSlideOperator();
+        stsOp.initByName(
+                "tree",tree,
+                "relSize",0.15,
+                "probCoalAttach", 1.0,
+                "weight", 1.0);
+
+        Node startNode = tree.getNode(0).getParent();
+        double lambda = stsOp.getCurrentLambda();
+
+        SubtreeSlideOperator.AttachmentPoint ap =
+                stsOp.getOlderAttachmentPoint(startNode, stsOp.getCurrentLambda());
+
+        System.out.println(tree);
         System.out.println(ap);
 
         double logP1 = ap.logProb;
